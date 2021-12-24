5d1c6ea0aa28c7fe37efdb35c6dcf66bd0d1165d
PrimenZzz <albertfantom11@gmail.com>
2021-12-24 11:06:16 +0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,13 +1,21 @@
 import pygame
 
+from settings import *
+
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, file_name, position):
+    def __init__(self, file_name, position, screen_height):
         super().__init__()
 
         self.image = pygame.image.load(file_name).convert_alpha()
         self.rect = self.image.get_rect(midbottom=position)
 
-        self.speed = 12
+        self.screen_height = screen_height
+
+        self.speed = bullet_speed
 
     def update(self):
-        self.rect.y -= self.speed
+        if self.rect.bottom >= 0:
+            self.rect.y -= self.speed
+        
+        else:
+            self.kill()
