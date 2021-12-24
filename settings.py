f4201650a77189cd56104fe7c6e0f9a2a6f87762
PrimenZzz <albertfantom11@gmail.com>
2021-12-24 16:33:47 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,28 @@
-player_speed = 6
+player_speed = 8
 bullet_speed = 12
-weapon_cooldown = 200
+weapon_cooldown = 400
 
 player_image = "images\\player.png"
 bullet_image = "images\\bullet.png"
+
+size_shape = 16
+
+# shape = [
+#     "  xxxxxxx  ",
+#     " xxxxxxxxx ",
+#     "xxxxxxxxxxx",
+#     "xxxxxxxxxxx",
+#     "xxxxxxxxxxx",
+#     "xxx     xxx",
+#     "xx       xx"
+# ]   
+
+shape = [
+    " xxxxxxxxx ",
+    "xxxxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxxxxxxxxxx",
+    "xxx     xxx",
+    "xx       xx"
+]   
