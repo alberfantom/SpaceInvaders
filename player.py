4141f2ed0b011c901f7414fa0aa9d764a1b8a64e
PrimenZzz <albertfantom11@gmail.com>
2021-12-26 18:24:12 +0300

    Update player.py

--- player.py
+++ player.py
@@ -7,28 +7,30 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, file_path, start_x, start_y, speed):
         super().__init__()
 
-        self.image = pygame.image.load(file_path).convert_alpha()
+        self.file_path = file_path
+
+        self.image = pygame.image.load(self.file_path).convert_alpha()
         self.rect = self.image.get_rect(midbottom=(start_x, start_y))
 
         self.speed = speed
 
-        self.weapon = Weapon(WEAPON_COOLDOWN)
+        self.weapon = Weapon(WEAPON_IMAGE, WEAPON_COOLDOWN, self, self.rect.center)
 
     def update(self):
-        self.movement()
+        self.movement(self.rect)
 
-    def movement(self):
+    def movement(self, object_rect):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            if self.rect.left >= 0:
-                self.rect.x -= self.speed
+            if object_rect.left >= 0:
+                object_rect.x -= self.speed
 
         if keys[pygame.K_d]:
-            if self.rect.right <= SCREEN_WIDTH:
-                self.rect.x += self.speed
+            if object_rect.right <= SCREEN_WIDTH:
+                object_rect.x += self.speed
 
-        if keys[pygame.SPACE]:
+        if keys[pygame.K_SPACE]:
             self.weapon.shoot()
 
         
