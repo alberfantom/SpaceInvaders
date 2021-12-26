6fc701830636e7bac6ec0917c2e808328e2e4b08
PrimenZzz <albertfantom11@gmail.com>
2021-12-26 18:24:18 +0300

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,15 +1,44 @@
 import pygame
 
-class Weapon:
+from settings import *
+
+class Weapon(pygame.sprite.Sprite):
     class Bullet(pygame.sprite.Sprite):
-        def __init__(self, file_path):
+        def __init__(self, file_path, position, speed):
             super().__init__()
-
             self.image = pygame.image.load(file_path).convert_alpha()
-            self.rect = self.image.get_rect()
+            self.rect = self.image.get_rect(midbottom=(position))
+            self.speed = speed
 
-    def __init__(self, cooldown, start_x, start_y):
+        def update(self):
+            if self.rect.bottom >= 0:
+                self.rect.y -= self.speed
+
+            else:
+                self.kill()
+
+    def __init__(self, file_path, cooldown, object, start_position):
+        super().__init__()
+        self.image = pygame.image.load(file_path).convert_alpha()
+        self.rect = self.image.get_rect(midbottom=(start_position))
+        self.object = object
         self.cooldown = cooldown
+        self.bullets = pygame.sprite.Group()
+        self.is_ready = True
+
+    def update(self):
+        self.object.movement(self.rect)
 
     def shoot(self):
-        pass
+        if self.is_ready:
+            self.time_shooted = pygame.time.get_ticks()
+            self.bullet_sprite = self.Bullet(BULLET_IMAGE, self.rect.center, BULLET_SPEED)
+            self.bullets.add(self.bullet_sprite)
+            self.is_ready = False
+        else:
+            self.recharge()
+
+    def recharge(self):
+        self.current_time = pygame.time.get_ticks()
+        if self.current_time - self.time_shooted >= self.cooldown:
+            self.is_ready = True
